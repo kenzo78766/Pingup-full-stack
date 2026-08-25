714b78e139097d1b4381b3ec39269f65d704d2d1
kenzo78766 <anshumanboard1@gmail.com>
2026-08-25 17:03:26 +0530

    Fix Inngest v4 functions

--- pingup-full-stack/server/inngest/index.js
+++ pingup-full-stack/server/inngest/index.js
@@ -10,17 +10,20 @@ export const inngest = new Inngest({ id: "pingup-app" });
 
 // Inngest Function to save user data to a database
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
-    async ({event})=>{
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
-        let username = email_addresses[0].email_address.split('@')[0]
+    {
+        id: "sync-user-from-clerk",
+        triggers: [{ event: "clerk/user.created" }]
+    },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+        let username = email_addresses[0].email_address.split("@")[0];
 
         // Check availability of username
-        const user = await User.findOne({username})
+        const user = await User.findOne({ username });
 
         if (user) {
-            username = username + Math.floor(Math.random() * 10000)
+            username = username + Math.floor(Math.random() * 10000);
         }
 
         const userData = {
@@ -29,49 +32,59 @@ const syncUserCreation = inngest.createFunction(
             full_name: first_name + " " + last_name,
             profile_picture: image_url,
             username
-        }
-        await User.create(userData)
-        
-    }
-)
+        };
 
-// Inngest Function to update user data in database 
+        await User.create(userData);
+    }
+);
+
+// Inngest Function to update user data in database
 const syncUserUpdation = inngest.createFunction(
-    {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated'},
-    async ({event})=>{
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
-        
-    const updatedUserData = {
-        email:  email_addresses[0].email_address,
-        full_name: first_name + ' ' + last_name,
-        profile_picture: image_url
+    {
+        id: "update-user-from-clerk",
+        triggers: [{ event: "clerk/user.updated" }]
+    },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+        const updatedUserData = {
+            email: email_addresses[0].email_address,
+            full_name: first_name + " " + last_name,
+            profile_picture: image_url
+        };
+
+        await User.findByIdAndUpdate(id, updatedUserData);
     }
-    await User.findByIdAndUpdate(id, updatedUserData)
-        
-    }
-)
+);
 
 // Inngest Function to delete user from database
 const syncUserDeletion = inngest.createFunction(
-    {id: 'delete-user-with-clerk'},
-    {event: 'clerk/user.deleted'},
-    async ({event})=>{
-        const {id} = event.data
-        await User.findByIdAndDelete(id)
+    {
+        id: "delete-user-with-clerk",
+        triggers: [{ event: "clerk/user.deleted" }]
+    },
+    async ({ event }) => {
+        const { id } = event.data;
+
+        await User.findByIdAndDelete(id);
     }
-)
+);
 
 // Inngest Function to send Reminder when a new connection request is added
 const sendNewConnectionRequestReminder = inngest.createFunction(
-    { id: "send-new-connection-request-reminder"},
-    {event: "app/connection-request"},
+    {
+        id: "send-new-connection-request-reminder",
+        triggers: [{ event: "app/connection-request" }]
+    },
     async ({ event, step }) => {
-        const {connectionId} = event.data;
+        const { connectionId } = event.data;
 
-        await step.run('send-connection-request-mail', async () => {
-            const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
+        await step.run("send-connection-request-mail", async () => {
+            const connection = await Connection.findById(connectionId)
+                .populate("from_user_id to_user_id");
+
             const subject = `👋 New Connection Request`;
+
             const body = `
             <div style="font-family: Arial, sans-serif; padding: 20px;">
                 <h2>Hi ${connection.to_user_id.full_name},</h2>
@@ -85,19 +98,23 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
                 to: connection.to_user_id.email,
                 subject,
                 body
-            })
-        })
+            });
+        });
 
-        const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000)
+        const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
         await step.sleepUntil("wait-for-24-hours", in24Hours);
-        await step.run('send-connection-request-reminder', async () => {
-            const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
 
-            if(connection.status === "accepted"){
-                return {message: "Already accepted"}
+        await step.run("send-connection-request-reminder", async () => {
+            const connection = await Connection.findById(connectionId)
+                .populate("from_user_id to_user_id");
+
+            if (connection.status === "accepted") {
+                return { message: "Already accepted" };
             }
 
             const subject = `👋 New Connection Request`;
+
             const body = `
             <div style="font-family: Arial, sans-serif; padding: 20px;">
                 <h2>Hi ${connection.to_user_id.full_name},</h2>
@@ -111,38 +128,50 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
                 to: connection.to_user_id.email,
                 subject,
                 body
-            })
+            });
 
-            return {message: "Reminder sent."}
-        })
+            return { message: "Reminder sent." };
+        });
     }
-)
+);
 
 // Inngest Function to delete story after 24 hours
 const deleteStory = inngest.createFunction(
-    {id: 'story-delete'},
-    { event: 'app/story.delete' },
+    {
+        id: "story-delete",
+        triggers: [{ event: "app/story.delete" }]
+    },
     async ({ event, step }) => {
         const { storyId } = event.data;
-        const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000)
-        await step.sleepUntil('wait-for-24-hours', in24Hours)
+
+        const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
+        await step.sleepUntil("wait-for-24-hours", in24Hours);
+
         await step.run("delete-story", async () => {
-            await Story.findByIdAndDelete(storyId)
-            return { message: "Story deleted." }
-        })
+            await Story.findByIdAndDelete(storyId);
+
+            return { message: "Story deleted." };
+        });
     }
-)
+);
 
+// Inngest Function to send notification of unseen messages
 const sendNotificationOfUnseenMessages = inngest.createFunction(
-    {id: "send-unseen-messages-notification"},
-    {cron: "TZ=America/New_York 0 9 * * *"}, // Every Day 9 AM
-    async ({step}) => {
-        const messages = await Message.find({seen: false}).populate('to_user_id');
-        const unseenCount = {}
+    {
+        id: "send-unseen-messages-notification",
+        triggers: [{ cron: "TZ=America/New_York 0 9 * * *" }]
+    },
+    async ({ step }) => {
+        const messages = await Message.find({ seen: false })
+            .populate("to_user_id");
 
-        messages.map(message=> {
-            unseenCount[message.to_user_id._id] = (unseenCount[message.to_user_id._id] || 0) + 1;
-        })
+        const unseenCount = {};
+
+        messages.map((message) => {
+            unseenCount[message.to_user_id._id] =
+                (unseenCount[message.to_user_id._id] || 0) + 1;
+        });
 
         for (const userId in unseenCount) {
             const user = await User.findById(userId);
@@ -163,14 +192,14 @@ const sendNotificationOfUnseenMessages = inngest.createFunction(
                 to: user.email,
                 subject,
                 body
-            })
+            });
         }
-        return {message: "Notification sent."}
+
+        return { message: "Notification sent." };
     }
-)
+);
 
-
-// Create an empty array where we'll export future Inngest functions
+// Export all Inngest functions
 export const functions = [
     syncUserCreation,
     syncUserUpdation,
